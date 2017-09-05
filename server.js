6dc34f928a498ab1e22f9db017ab0d8900976204
System <none>
2017-09-05 15:29:58 +0000

    add endpoints

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express');
-var dateformat = require('dateformat');
+var moment = require('moment');
 var app = express();
 app.use(express.static('public'));
+var months = ['January',
+             'February',
+             'March',
+             'April',
+             'May',
+             'June',
+             'July',
+             'August',
+             'September',
+             'October',
+             'November',
+             'December'];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.get("/:time", function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var date;
+  var dateObj = {unix: null, natural: date};
+  res.send(JSON.stringify(dateObj));
+})
+
+app.listen(process.env.PORT,);
